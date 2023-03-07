00574a5ccf988068b487ea52eb5a66c49e980dae
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-03-07 09:16:36 +0000

    removed / moved decorators to dedicated package to avoid es6 compatible constructor errors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-// Export decorators
-export { Reducer, State } from './decorators';
 // Export hooks
 export {
   useCompletableRxEffect,
   useRxEffect,
   useRxReducer,
-  useRxState
+  useRxState,
 } from './hooks';
-// Export types
-export {
-  DispatchType,
-  ReducerComponentType,
-  SetStateType,
-  StateComponentType
-} from './types';
-
